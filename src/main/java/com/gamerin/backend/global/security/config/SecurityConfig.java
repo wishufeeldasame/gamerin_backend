1bcbaa12f9f1d8f0b1ead456229fffe86258ce9c
WhiteB1ossom <blossom5831@naver.com>
2026-04-13 19:17:27 +0900

    프론트/백 로그인 병합

--- src/main/java/com/gamerin/backend/global/security/config/SecurityConfig.java
+++ src/main/java/com/gamerin/backend/global/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.gamerin.backend.global.security.config;
 
 import com.gamerin.backend.global.security.jwt.JwtAuthenticationFilter;
 import com.gamerin.backend.global.security.oauth2.OAuth2SuccessHandler;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -50,11 +52,19 @@ public class SecurityConfig {
                                 "/api/v1/auth/social-signup",
                                 "/api/v1/auth/login",
                                 "/api/v1/auth/refresh",
+                                "/api/v1/auth/logout",
                                 "/api/v1/auth/availability/**",
                                 "/oauth2/**",
                                 "/login/oauth2/**"
                         ).permitAll()
                         .anyRequest().authenticated()
+                )
+                .exceptionHandling(exception -> exception
+                        .defaultAuthenticationEntryPointFor(
+                                (request, response, authException) ->
+                                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED),
+                                new AntPathRequestMatcher("/api/**")
+                        )
                 )
                 .oauth2Login(oauth2 -> oauth2
                             .successHandler(oAuth2SuccessHandler))
